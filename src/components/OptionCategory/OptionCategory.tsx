4182f4566148d56452af761638a36f8919eeaf45
Ruth <elith.tati@gmail.com>
2025-04-30 11:45:45 -0300

    Añadiendo componentes de la screen catalogo

---
+++ src/components/OptionCategory/OptionCategory.tsx
@@ -0,0 +1,15 @@
+import { FC } from 'react'
+import styles from './OptionCategory.module.css'
+interface Prop {
+    src: string,
+    alt: string
+    text: string
+}
+export const OptionCategory: FC<Prop> = ({ src, alt, text }) => {
+    return (
+        <article className={styles.containerOptionCategory}>
+            <img className={styles.imageBackgraund} src={src} alt={alt} />
+            <p className={styles.text}>{text}</p>
+        </article>
+    )
+}
---
+++ src/pages/ProductCatalog/Products/Products.tsx
@@ -0,0 +1,9 @@
+import styles from './Products.module.css'
+
+export const Products = () => {
+  return (
+    <div className={styles.container}>
+        {/*  Item producto walter  */}
+    </div>
+  )
+}
