ce96ead7112e96efe8776c9a66697add4329b0e7
Ruth <elith.tati@gmail.com>
2025-05-06 15:51:33 -0300

    vista responsive del header

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -3,20 +3,30 @@ import carrito from "../../assets/svg/shop.svg";
 import { Link } from "react-router";
 import { useState } from "react";
 import { Modal } from "../ModalLoginRegister/Modal";
-
-
+import logo from "../../assets/images/myb.png";
+import menu from "../../assets/svg/menu.svg";
 
 export const Header = () => {
+  //Estado para abrir el modal
+  const [openModal, setOpenModal] = useState(false);
+  //Estado para la vista de la hamburgesa
+  const [menuOpen, setMenuOpen] = useState(false);
 
-  const [openModal, setOpenModal] =useState(false);
 
 
 
   return (
     <header className={styles.containerHeader}>
-      <img src="/logo.svg" className={styles.headerLogo} />
+      <button
+        className={styles.burgerButton}
+        onClick={() => setMenuOpen(!menuOpen)}
+      >
+        <img src={menu} alt="Menu" />
+      </button>
+      <img src={logo} className={styles.headerLogo} />
+
       <nav className={styles.headerOptions}>
-        <div className={styles.headerNavSearch}>
+        <div className={`${styles.headerNavSearch} ${menuOpen ? styles.showMenu : ''}`}>
           <Link to="/">Home</Link>
           <Link to="/catalogo/mujer">Mujer</Link>
           <Link to="/catalogo/hombre">Hombre</Link>
@@ -25,7 +35,7 @@ export const Header = () => {
           <Link to="/carrito">
             <img src={carrito} alt="Carrito de compras" />
           </Link>
-          <button onClick={()=> setOpenModal(true)}>Login</button> 
+          <button onClick={() => setOpenModal(true)}>Login</button>
         </div>
       </nav>
       {openModal && <Modal></Modal>}
