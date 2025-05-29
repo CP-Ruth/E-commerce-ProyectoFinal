7fb20bab103bc92ca0c88e5579a615565ed21f21
Ruth <elith.tati@gmail.com>
2025-05-29 17:33:23 -0300

    borre comentarios

--- src/hooks/useFilter.ts
+++ src/hooks/useFilter.ts
@@ -6,27 +6,14 @@ export const useFilter = () => {
   const [productos, setProductos] = useState<IDetailsProduct[]>([]);
   //   const [filterProducts, setFilterProducts] = useState<IDetailsProduct[]>([]);
 
-  const getProducts = async (sexo: String, filter: String) => {
+  const getProducts = async (sexo: String, tpProduct: String) => {
     const response = await getDetailsByFilters(
-      `sexo=${sexo}${filter && "&tipoProducto=" + filter}`
+      `sexo=${sexo}${tpProduct && "&tipoProducto=" + tpProduct}`
     );
     setProductos(response);
   };
 
-  //   const filterByPrice = (minPrice: number, maxPrice: number) => {
-  //     const filteredProducts = productos.filter(
-  //       ({ producto: { precio_venta } }) =>
-  //         precio_venta <= maxPrice && precio_venta >= minPrice
-  //     );
-  //     setFilterProducts(filteredProducts);
-  //   };
-
-  //   const filterByDescuento = (id: number) => {
-  //     const filteredProducts = productos.filter(
-  //       ({ descuento }) => descuento.id === id
-  //     );
-  //     setFilterProducts(filteredProducts);
-  //   };
+  
 
   return {
     productos,
